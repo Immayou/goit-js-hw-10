e0744bc9955e624969e87742cadd12bee25156fa
Yuliya Zozulya <djulia@ukr.net>
2022-09-01 01:06:13 +0300

    Задає шаблон виконання завдання

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
+import debounce from "lodash.debounce";
+import handlebars from "handlebars";
+import Notiflix from "notiflix";
 
 const DEBOUNCE_DELAY = 300;
+const countryInput = document.querySelector('#search-box')
+const countriesListShown = document.querySelector('.country-list')
+const countriesInfoShown = document.querySelector('.country-info')
+
+countryInput.addEventListener('input', debounce(onSearchCountryInput, DEBOUNCE_DELAY))
+
+function onSearchCountryInput(evt) {
+evt.preventDefault()
+const inputValue = evt.target.value.trim()
+
+fetchCountries(inputValue).then(createCountriesHtmlList)
+
+}
+
+function fetchCountries(name) {
+return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+.then(response => {return response.json()})
+}
+
+function createCountriesHtmlList ({name, capital, population, flags, languages}) {
+
+countriesListShown.insertAdjacentHTML('beforeend', `${name, capital, population, flags, languages}`)
+}
+
+
+
+    
+    
+   

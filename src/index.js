895b100695fefcc052fcdb07065504fe6d4d821d
Yuliya Zozulya <djulia@ukr.net>
2022-09-01 23:52:15 +0300

    Встановлює повідомлення з бібліотеки Notiflix

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ function onSearchCountryInput(evt) {
 evt.preventDefault()
 const inputValue = evt.target.value.trim()
 
-fetchCountries(inputValue).then(createCountriesHtmlList)
-
+if (inputValue !== '') {
+    fetchCountries(inputValue).then(createCountriesHtmlList).catch(notFoundCountry)
+} else {
+    countriesInfoShown.innerHTML = ''
+    countriesListShown.innerHTML = ''
+}
 }
 
 function fetchCountries(name) {
@@ -24,14 +28,32 @@ return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,po
 }
 
 function createCountriesHtmlList (countries) {
-countries.map(({name, capital, population, flags, languages}) => 
-{const template = 
-`<li class="countries-item"><svg width="40" height="40"><use href=${flags.svg}></use></svg><span>${name.official}</span></li>`
-    countriesListShown.insertAdjacentHTML('beforeend', template)})
+countries.map(({name, capital, population, flags, languages}) => {
+    const templateCountry = 
+    `<li class="country-item">
+    <img width="40" height="30" src=${flags.svg} alt="Flag" class="image-flag">
+    <span>${name.official}</span></li>`
+    countriesListShown.insertAdjacentHTML('beforeend', templateCountry)})
 
-
+if (countriesListShown.children.length === 1) {
+    createCountriesHtmlInfo (countries) 
+} else if (countriesListShown.children.length > 10) {
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+} 
 }
 
+
+function createCountriesHtmlInfo (country) {
+    const templateInfo = 
+    `<p class="country-info">Capital: ${country[0].capital}</p>
+     <p class="country-info">Population: ${country[0].population}</p>
+     <p class="country-info">Languages: ${Object.values(country[0].languages)}</p>`
+    countriesInfoShown.insertAdjacentHTML('beforeend', templateInfo)
+    }
+    
+function notFoundCountry () {
+    Notiflix.Notify.failure("Oops, there is no country with that name")
+}
 
 
     
